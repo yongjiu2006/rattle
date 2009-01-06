eb596272c4cd309e2b87503bd2dbacc1fb76c4b2
gjwkayon <gjwkayon@a39f102b-0e1a-0410-b7cc-512aa4d05af1>
2009-01-06 20:47:50 +0000

    Conditionally remove load time welcome message

--- trunk/src/zzz.R
+++ trunk/src/zzz.R
@@ -12,13 +12,16 @@
   # TODO: How to not do this if quietly=TRUE? Otherwise it will be
   # annoying, just like fBasics. randomForest seems to do it
   # correctly? Maybe not....
-  
-  cat(sprintf(paste("Rattle, Graphical interface for data mining using R\n",
-                    "Version %s. ", COPYRIGHT, "\n", sep=""), VERSION))
-  if ("rattle" %in% getOption("defaultPackages"))
-    rattle()
-  else
-    cat(paste("Type \"rattle()\" to shake, rattle, and roll ",
-              "your data.\n", sep=""))
+
+  if (! (exists("isrstat") && isrstat))
+  {
+    cat(sprintf(paste("Rattle, Graphical interface for data mining using R\n",
+                      "Version %s. ", COPYRIGHT, "\n", sep=""), VERSION))
+    if ("rattle" %in% getOption("defaultPackages"))
+      rattle()
+    else
+      cat(paste("Type \"rattle()\" to shake, rattle, and roll ",
+                "your data.\n", sep=""))
+  }
 }
 
