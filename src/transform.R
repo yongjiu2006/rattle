56066be425bbcf5234b2604dbb85f1e574366c4f
gjwkayon <gjwkayon@a39f102b-0e1a-0410-b7cc-512aa4d05af1>
2009-01-07 20:49:46 +0000

    Add test for transform support to pmml - release pmml 1.2.2

--- src/transform.R
+++ src/transform.R
@@ -1,6 +1,6 @@
 # Gnome R Data Miner: GNOME interface to R for Data Mining
 #
-# Time-stamp: <2009-01-07 16:40:14 Graham Williams>
+# Time-stamp: <2009-01-08 07:46:09 Graham Williams>
 #
 # TRANSFORM TAB
 #
@@ -93,20 +93,6 @@ modalvalue <- function(x, na.rm=FALSE)
     }
   }
   u[which.max(frequencies)]
-}
-
-supportTransformExport <- function(transforms=NULL)
-{
-  # 090107 Returns TRUE/FALSE. Make sure the required functions for
-  # generating transforms in the PMML are available in some form
-  # (either packaged with Rattle or else loaded afterwards) and that
-  # there are transforms to be handled. Another alternative would be
-  # to define dummy functions that can be overridden, but that
-  # requires more design.
-  
-  return(length(getAnywhere("pmml.transforms")$objs) > 0 &&
-         length(getAnywhere("unifyTransforms")$objs) > 0 &&
-         ! is.null(transforms))
 }
 
 ########################################################################
