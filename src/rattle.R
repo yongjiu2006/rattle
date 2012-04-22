3dd954935e6718c8962ab62a06ff07960b6fa25e
gjwkayon <gjwkayon@a39f102b-0e1a-0410-b7cc-512aa4d05af1>
2012-04-22 11:51:56 +0000

    Remove old function doc

--- src/rattle.R
+++ src/rattle.R
@@ -28,7 +28,7 @@ Rtxt <- function(...)
 RtxtNT <- Rtxt
 
 VERSION <- "2.6.18"
-DATE <- "2012-04-21"
+DATE <- "2012-04-22"
 # 091223 Rtxt does not work until the rattle GUI has started, perhaps?
 COPYRIGHT <- paste(Rtxt("Copyright"), "(C) 2006-2011 Togaware Pty Ltd.")
 
